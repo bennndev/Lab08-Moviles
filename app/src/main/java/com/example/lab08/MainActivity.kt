bc0c754803673b423f9964493d2337149ab1005d
neB <benjamin.sullca1103@gmail.com>
2025-10-07 14:48:59 -0500

    feat: espaciado entre la pantalla y la barra superior del dispositivo.

--- app/src/main/java/com/example/lab08/MainActivity.kt
+++ app/src/main/java/com/example/lab08/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -65,6 +66,7 @@ fun TaskScreen(viewModel: TaskViewModel) {
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .statusBarsPadding()
             .padding(16.dp)
     ) {
         TextField(
